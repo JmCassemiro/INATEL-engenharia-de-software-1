3b381a88591da9eba6ac407e5d632dc5b961a8f2
Thomas Victor <thomasvictor@Thomass-MacBook-Pro.local>
2025-08-18 23:52:26 -0300

    adicionando novo projeto

---
+++ flagit/flag_api.py
@@ -0,0 +1,9 @@
+import random
+import httpx
+
+async def get_random_flag():
+    url = "https://restcountries.com/v3.1/all?fields=name,flags"
+    async with httpx.AsyncClient() as client:
+        res = await client.get(url)
+        countries = res.json()
+    return countries
--- flagit/routes.py
+++ flagit/routes.py
@@ -1,7 +1,42 @@
-from fastapi import APIRouter
+import random
+from fastapi import APIRouter, Request
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
 
+from flag_api import get_random_flag
+
+templates = Jinja2Templates(directory="templates")
 router = APIRouter()
 
-@router.get("/")
-async def home():
-    return {"message": "Welcome to the FlagIt API!"}
+
+@router.get("/", response_class=HTMLResponse)
+async def home(request: Request):
+    flag_data = await get_random_flag()
+
+    random_number = random.randint(0, len(flag_data) - 1)
+    flag = flag_data[random_number]["flags"]["png"]
+    name = flag_data[random_number]["name"]["common"]
+
+    response = templates.TemplateResponse(
+        "index.html",
+        {"request": request, "flag_url": flag, "streak": 0, "message": ""},
+    )
+
+    response.set_cookie(key="correct_country", value=name)
+
+    return response
+
+
+@router.post("/guess")
+async def guess(request: Request):
+    data = await request.json()
+    correct = request.cookies.get("correct_country")
+    print(correct.lower())
+
+    if data["answer"].lower() == correct.lower():
+        message = "Parabéns! Você acertou!"
+    else:
+        message = f"Errado! A resposta correta era {correct}."
+
+    answer = data.get("answer")
+    return {"message": message, "answer": answer}
