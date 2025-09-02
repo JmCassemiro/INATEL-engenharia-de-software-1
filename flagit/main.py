032a5f9ad2b9176e433963268b06ea90c2439b6b
Thomas Victor <thomasvictordias@outlook.com>
2025-09-02 18:48:32 -0300

    adicionando static no app

--- flagit/main.py
+++ flagit/main.py
@@ -1,4 +1,3 @@
-import os
 from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
 import uvicorn
@@ -8,6 +7,7 @@ app = FastAPI()
 from routes import router
 
 app.include_router(router)
+app.mount("/static", StaticFiles(directory="static"), name="static")
 
 if __name__ == "__main__":
     uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
