fcb2db7b7131dd8519fe9352a00dc47a1499e330
Thomas Victor <thomasvictordias@outlook.com>
2025-09-02 16:18:13 -0300

    ajustes no codigo

---
+++ flagit/main.py
@@ -0,0 +1,13 @@
+import os
+from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
+import uvicorn
+
+app = FastAPI()
+
+from routes import router
+
+app.include_router(router)
+
+if __name__ == "__main__":
+    uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
---
+++ flagit/routes.py
@@ -0,0 +1,52 @@
+from fastapi import APIRouter, Request
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
+
+from flag_api import get_random_flag, get_countries_name
+
+import os
+from fastapi.templating import Jinja2Templates
+
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+templates = Jinja2Templates(directory=os.path.join(BASE_DIR, "templates"))
+router = APIRouter()
+
+
+@router.get("/", response_class=HTMLResponse)
+async def home(request: Request):
+    country = await get_random_flag()
+
+    # renderiza o template com a bandeira
+    response = templates.TemplateResponse(
+        request,
+        "index.html",
+        {"flag_url": country["flag"]},
+    )
+
+    # seta o cookie com o país correto
+    response.set_cookie(key="correct_country", value=country["name"])
+
+    return response
+
+
+@router.post("/guess")
+async def guess(request: Request):
+    data = await request.json()
+    correct_flag = request.cookies.get("correct_country")
+
+    # verifica se o palpite está correto
+    is_guess_correct = False
+    if data["answer"].lower() == correct_flag.lower():
+        message = "Parabéns! Você acertou!"
+        is_guess_correct = True
+    else:
+        correct_flag = correct_flag
+
+    # retorna se o palpite está correto junto com o país correto
+    return {"correct_flag": correct_flag, "is_guess_correct": is_guess_correct}
+
+
+@router.get("/get_names")
+async def get_names():
+    countries = await get_countries_name()
+    return countries
